ff41dc41ffa022daa76e260c615b89e7d0257d28
Kyle Orr <KORR202@caledonian.ac.uk>
2021-10-12 10:59:04 +0100

    This is my first project in Java

---
+++ src/com/softwareinstitute/training/orr/kyle/Main.java
@@ -0,0 +1,50 @@
+package com.softwareinstitute.training.orr.kyle;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Scanner HeightScan = new Scanner(System.in);
+        System.out.println("Enter Wall Height: ");
+        double heightinput = HeightScan.nextDouble();
+
+        Scanner WidthScan = new Scanner(System.in);
+        System.out.println("Enter Wall width: ");
+        double widthinput = WidthScan.nextDouble();
+
+        String Height = HeightScan.nextLine();
+        System.out.println("Wall Height is " + heightinput);
+
+        String Width = WidthScan.nextLine();
+        System.out.println("Wall Width is " + widthinput);
+
+        double wallsize = heightinput * widthinput;
+        System.out.println("Wall size is " + wallsize);
+
+        double PaintPotVol = 1; //Assumption that the paint pot is 1 Litre!
+        double PaintPot = 6; //Assumption that one pot covers 6 Metres Squared!
+
+        Scanner PaintPotVl = new Scanner(System.in);
+        System.out.println("How much paint is in the pot in litres: ");
+        double paintpotinput = PaintPotVl.nextDouble();
+
+        double PaintPotCover;
+        PaintPotCover = paintpotinput/6; //Assumption that one litre covers 6 metres squared!
+
+        double NumberOfCans;
+        NumberOfCans = Math.ceil(wallsize/PaintPotCover);
+        System.out.println("Number of cans needed is " + NumberOfCans);
+
+        Scanner CostScan = new Scanner(System.in);
+        System.out.println("What is the cost of the can: ");
+        double costinput = CostScan.nextDouble();
+
+        double CostToPaint;
+        CostToPaint = costinput * NumberOfCans;
+        System.out.println("The cost to paint is " + CostToPaint);
+    }
+
+}
+
